ab1f6f9685cd355a994f79668fe58a137849c9cc
Gayathri Dinesh <gaychidinesh@gmail.com>
2025-11-20 12:14:34 +0400

    Update app.py

--- app.py
+++ app.py
@@ -304,5 +304,5 @@ if __name__ == '__main__':
         print(f"Features: {X_train_global.shape[1]}")
         
     import os
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
